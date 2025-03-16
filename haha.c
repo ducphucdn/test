827d9c5539e1534a7ec4d087e4f103dcf4942939
baolam <lam01662052827@gmail.com>
2025-03-16 21:58:29 +0700

    Haha feature

--- haha.c
+++ haha.c
@@ -3,12 +3,13 @@
 int main()
 {
     printf("Thu chich dien\n");
-    printf("Ong co muon chich dien khong Phuc ui?");
+    printf("Ong co muon chich dien khong Phuc ui? \n");
     printf("0. Khong 1. Co \n");
 
     int state = 0;
     do
     {
+        printf("Nhap lua chon cua ban: ");
         scanf("%d", &state);
     } while (state != 1);
 
