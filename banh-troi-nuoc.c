9cebf5db97afcabf930d0d1995b117c36b065600
Huy Phong <touman876@gmail.com>
2025-03-16 21:58:03 +0700

    Tho hay

---
+++ banh-troi-nuoc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("<==Banh troi nuoc==>\n");
+    printf("Than em vua trang lai vua tron\n");
+    printf("Bay noi ba chim voi nuoc non\n");
+    printf("Ran nat mac dau tay ke nan\n");
+    printf("Nhung em van giu tam long son\n");
+
+    return 0;
+}
